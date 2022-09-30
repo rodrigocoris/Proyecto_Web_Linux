24929c42058a97169b02277dbe653b2066522b6f
RodrigoRamosL <ramoslozanorodrigoalejandro@gmail.com>
2022-09-30 12:18:50 -0500

    commit de base de datos funcionando

---
+++ sesion.php
@@ -0,0 +1,45 @@
+<?php 
+
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$db_name="formulariosesion";
+
+
+$con =mysqli_connect($db_host, $db_user, $db_password,$db_name);
+
+if(!$con){
+    die("Error " . mysqli_connect_error());
+}else{
+    echo "conectado..";
+}
+
+
+//Usuario
+//iformacion a agregar
+
+$color = $_POST["color"];
+$marca = $_POST["marca"];
+$modelo = $_POST["modelo"];
+$precio = $_POST["precio"];
+$transmision = $_POST["transmision"];
+
+//Consulta en la base de datos
+
+$inser = "INSERT INTO usuario(id, color, marca, modelo, precio, transmision)
+    values ('0', '$color','$marca','$modelo','$precio','$transmision')";
+
+    $ir=mysqli_query($con,$inser);
+
+    if($ir){
+        echo "se ha registrado con exito";
+
+    }else{
+
+        echo "hay un error";
+    }
+
+
+
+    mysqli_close($con)
+?>
