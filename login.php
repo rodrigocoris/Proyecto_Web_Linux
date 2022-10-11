c31eec722c1dc23e9c8829dc14b9a2228e9fd10f
RodrigoRamosL <ramoslozanorodrigoalejandro@gmail.com>
2022-10-11 13:48:10 -0500

    Commit de login

---
+++ login.php
@@ -0,0 +1,45 @@
+<?php 
+
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$db_name="formulariosesion";
+
+
+$con =mysqli_connect($db_host, $db_user, $db_password,$db_name);
+
+if(!$con){
+    die("Error " . mysqli_connect_error());
+}else{
+    echo "conectado..";
+}
+
+
+//Usuario
+//iformacion a agregar
+
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$sexo = $_POST["sexo"];
+$contrasena = $_POST["contrasena"];
+
+//Consulta en la base de datos
+
+$inser = "INSERT INTO loginini(id, nombre, correo, telefono, sexo, contrasena)
+    values ('0', '$nombre','$correo','$telefono','$sexo','$contrasena')";
+
+    $ir=mysqli_query($con,$inser);
+
+    if($ir){
+        echo "se ha registrado con exito";
+
+    }else{
+
+        echo "hay un error";
+    }
+
+
+
+    mysqli_close($con)
+?>
