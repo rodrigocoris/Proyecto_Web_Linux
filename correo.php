1ab478cdac8b9a6591f16aacf6f0563a3c3a9f5a
RodrigoRamosL <ramoslozanorodrigoalejandro@gmail.com>
2022-12-05 19:31:36 -0600

    Proyecto terminado

--- correo.php
+++ correo.php
@@ -1,8 +1,9 @@
 <?php
 $para      = 'ramoslozanorodrigoalejandro@gmail.com';
-$asunto    = 'Pedido realizado con exito numero de registro: #216';
+$asunto    = 'Pedido realizado con exito numero de Id de seguimiento: #21624156';
 $descripcion   = 'Gracias por tu compra, Legendary motorsport agradece su preferencia, dentro de dos dias
-habiles te llegara otro correo con toda la informacion de tu compra! que tengas un excelente dia ';
+habiles te llegara otro correo con toda la informacion de tu compra, esperamos que tengas un excelente dia y gracias por ser parte
+de la familia Legendary Motorsport';
 $de = 'From: a21110135@ceti.mx';
 
 if (mail($para, $asunto, $descripcion, $de))
